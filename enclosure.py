accd5aa754ce1e62a47c5adb89423dc7a4d6bb21
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-11-20 18:54:37 +1030

    Create enclosure class and add attributes/constants.

--- enclosure.py
+++ enclosure.py
@@ -5,4 +5,17 @@ Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Enclosure:
+    CLEANLINESS = {"Clean", "Moderate", "Dirty"}
+    ENVIRONMENTS = {"Savannah", "Aquatic", "Aviary", "Jungle", "Desert", "Arctic"}
+    def __init__(self, name, size, environmental_type, cleanliness_level, compatible_species):
+        self.__name = name
+        self.__environmental_type = environmental_type
+        self.__size = size
+        self.__cleanliness = cleanliness_level
+        self.__compatible_species = compatible_species
+        self.__animal_species = []
+
+
